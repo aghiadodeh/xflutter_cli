3c5f3cbaee7716e8c3c33696c97f1c493c15d340
Aghiad Odeh <aghiadodeh@gmail.com>
2024-09-05 19:28:34 +0300

    add generic type "T" to EventEmitter "observe" method

--- xflutter_cli_test_application/lib/events/event_emitter.dart
+++ xflutter_cli_test_application/lib/events/event_emitter.dart
@@ -7,28 +7,43 @@ typedef EventEmitterObserver<T> = Function(T value);
 
 class EventEmitter<T> {
   StreamSubscription? _streamSubscription;
-  final _observers = List<EventEmitterObserver>.empty(growable: true);
+  final _observers = List<EventEmitterObserver<T>>.empty(growable: true);
   final _controller = StreamController<T>();
+
+  EventEmitter() {
+    _streamSubscription = result.listen((T event) {
+      _listen(event);
+    });
+  }
+
+  void _listen(T event) {
+    for (final observer in _observers) {
+      observer(event);
+    }
+  }
 
   Stream<T> get result async* {
     yield* _controller.stream;
   }
 
-  void emit(T Function() value) {
-    _controller.add(value.call());
+  void emit(T Function() function) {
+    _controller.add(function.call());
+  }
+
+  Future<void> emitAsync(Future<T> Function() function) async {
+    _controller.add(await function.call());
   }
 
   void emitValue(T value) {
     _controller.add(value);
   }
 
-  void observe(EventEmitterObserver callback) {
+  Future<void> emitAsyncValue(Future<T> value) async {
+    _controller.add(await value);
+  }
+
+  void observe(EventEmitterObserver<T> callback) {
     _observers.add(callback);
-    _streamSubscription = result.listen((event) {
-      for (final observer in _observers) {
-        observer(event);
-      }
-    });
   }
 
   void dispose() {
