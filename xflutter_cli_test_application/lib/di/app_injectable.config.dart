0fcb968c582a9451bb390c9a822567e16d103fb1
Aghiad Odeh <aghiadodeh@gmail.com>
2024-11-04 11:52:29 +0200

    add home screen

--- xflutter_cli_test_application/lib/di/app_injectable.config.dart
+++ xflutter_cli_test_application/lib/di/app_injectable.config.dart
@@ -15,6 +15,7 @@ import 'package:shared_preferences/shared_preferences.dart' as _i460;
 import 'package:xflutter_cli_test_application/di/modules/app_module.dart' as _i596;
 import 'package:xflutter_cli_test_application/environments/environments.dart' as _i446;
 import 'package:xflutter_cli_test_application/ui/resources/themes/theme_notifier.dart' as _i0;
+import 'package:xflutter_cli_test_application/ui/screens/home/viewmodels/home_viewmodel.dart' as _i139;
 import 'package:xflutter_cli_test_application/utilities/network/connectivity.dart' as _i1037;
 
 const String _dev = 'dev';
@@ -53,6 +54,17 @@ extension GetItInjectableX on _i174.GetIt {
       registerFor: {_prod},
     );
     return this;
+  }
+
+// initializes the registration of home-scope dependencies inside of GetIt
+  _i174.GetIt initHomeScope({_i174.ScopeDisposeFunc? dispose}) {
+    return _i526.GetItHelper(this).initScope(
+      'home',
+      dispose: dispose,
+      init: (_i526.GetItHelper gh) {
+        gh.lazySingleton<_i139.HomeViewModel>(() => _i139.HomeViewModel());
+      },
+    );
   }
 }
 
--- xflutter_cli_test_application/lib/router/app_router.gr.dart
+++ xflutter_cli_test_application/lib/router/app_router.gr.dart
@@ -8,3 +8,22 @@
 // coverage:ignore-file
 
 part of 'app_router.dart';
+
+/// generated route for
+/// [HomeScreen]
+class Home extends PageRouteInfo<void> {
+  const Home({List<PageRouteInfo>? children})
+      : super(
+          Home.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'Home';
+
+  static PageInfo page = PageInfo(
+    name,
+    builder: (data) {
+      return const HomeScreen();
+    },
+  );
+}
